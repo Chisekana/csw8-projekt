31f4d1d8870fcaef33f95696bd86d76721b2f507
Chisekana <okaloinnocent@gmail.com>
2023-06-12 13:45:17 +0300

    the_menu & Quit options added

--- step02/main.py
+++ step02/main.py
@@ -1,78 +1,30 @@
 from functions import *
 
 if __name__ == "__main__":
-
-    the_menu = {
+    the_menu = {    
     "L" : "List",
     "A" : "Add",
     "U" : "Update",
     "D" : "Delete",
-    "M" : "Show average price",
+    "H" : "Display restaurant expense rating",
     "S" : "Save the data to file",
     "R" : "Restore data from file",
     "Q" : "Quit this program"
 }
-    
-    restaurant_menu_list = [
-  {
-    "name": "ugali",
-    "calories": 500,
-    "price": 12.50,
-    "is_vegetarian": "no",
-    "spicy_level": 1
-  },
-  {
-    "name": "pilau bowl",
-    "calories": 400,
-    "price": 18.90,
-    "is_vegetarian": "yes",
-    "spicy_level": 3
-  },
-  {
-    "name": "nyama",
-    "calories": 800,
-    "price": 25.90,
-    "is_vegetarian": "no",
-    "spicy_level": 4
-  },
-  {
-    "name": "kienyeji",
-    "calories": 250,
-    "price": 5.90,
-    "is_vegetarian": "yes",
-    "spicy_level": 2
-  }
-]
-
-    list_menu = {
-        "A": "complete menu",
-        "V": "vegetarian dishes only",
-    }
-
-    spicy_scale_map = {
-        1: "Not spicy",
-        2: "Low key spicy",
-        3: "Hot",
-        4: "Diabolical",
-    }
- 
-    
     opt = None
 
     while True:
-        def print_main_menu(menu):
-            print("Welcome to Alinjos Restaurant!")
-        opt = input("How may I assist you ?")
+        def print_main_menu(the_menu):
+            print()
+        opt = input("Welcome. What would you like to Order?")
 
         if opt == "Q" or opt == "q":
             print("Goodbye!\n")
-            break
+            break # exit the main `while` loop
         else:
-            if opt == "L" or "A" or "U" or "D" or "M" or "S" or "R":
-                print(f"You selected option {opt} to > {the_menu[opt]}")
+            if opt != "Q" or opt != "q" in the_menu: # TODO 3: check of the character stored in opt is in the_menu dictionary
+                print(f"You selected option {opt} to > {the_menu[opt]}.")
             else:
                 print(f"WARNING: {opt} is an invalid option.\n")
-            #list_helper(list_menu, restaurant_menu_list, spicy_scale_map)
-
 
     print("Have a delicious day!")
