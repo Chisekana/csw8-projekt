459313fb79b7a5427249184a45cd80c203936854
Chisekana <okaloinnocent@gmail.com>
2023-07-04 12:52:30 +0300

    introduced Assert statements

--- step02/tests.py
+++ step02/tests.py
@@ -1,1 +1,8 @@
 from functions import *
+from main import *
+
+assert opt == "Q" or opt == 'q', "Input option Q/q breaks the loop and prints 'Goodbye!'"
+
+assert opt in the_menu and opt != "Q" and opt != 'q', "Valid options prints corresponding menu items."
+
+assert opt not in the_menu and opt != "Q" and opt != 'q', "Invalid option displays warning message."
