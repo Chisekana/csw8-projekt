a9e88e9b71802ade6cb05a0c075df27f2d7303ff
Chisekana <okaloinnocent@gmail.com>
2023-07-04 12:47:49 +0300

    Updated the main menu

--- step02/main.py
+++ step02/main.py
@@ -1,30 +1,31 @@
 from functions import *
 
 if __name__ == "__main__":
-    the_menu = {    
-    "L" : "List",
-    "A" : "Add",
-    "U" : "Update",
-    "D" : "Delete",
-    "H" : "Display restaurant expense rating",
-    "S" : "Save the data to file",
-    "R" : "Restore data from file",
-    "Q" : "Quit this program"
-}
-    opt = None
+    the_menu = {
+        "L" : "List",
+        "A" : "Add",
+        "U" : "Update",
+        "D" : "Delete",
+        "M" : "Show average price",
+        "S" : "Save the data to file",
+        "R" : "Restore data from file",
+        "Q" : "Quit this program"
+    }
 
     while True:
         def print_main_menu(the_menu):
-            print()
-        opt = input("Welcome. What would you like to Order?")
+            print("Welcome to Alinjos Restaurant!")
+            opt = input("Choose from our Menu: ").upper()
 
-        if opt == "Q" or opt == "q":
-            print("Goodbye!\n")
-            break # exit the main `while` loop
-        else:
-            if opt != "Q" or opt != "q" in the_menu: # TODO 3: check of the character stored in opt is in the_menu dictionary
-                print(f"You selected option {opt} to > {the_menu[opt]}.")
+            if opt == "Q" or opt == 'q':
+                print("Goodbye!\n")
             else:
-                print(f"WARNING: {opt} is an invalid option.\n")
+                if opt in the_menu and opt != "Q" and opt != 'q':
+                    print(f"You selected option {opt} to > {the_menu[opt]}.")
+                else:
+                    print(f"WARNING: {opt} is an invalid option.\n")
+
+        print_main_menu(the_menu)
 
     print("Have a delicious day!")
+
