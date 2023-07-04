ab88a8a69c32bd163c5e3f5ff2bb7ff6220fbef6
Chisekana <okaloinnocent@gmail.com>
2023-07-04 12:50:46 +0300

    Updated all relevant Functions

--- step02/functions.py
+++ step02/functions.py
@@ -1,4 +1,20 @@
 # functions.py: function definitions for csw8 final project
+from main import *
 
-def print_main_menu(menu):
-    print()
+the_menu = {
+    "L" : "List",
+    "A" : "Add",
+    "U" : "Update",
+    "D" : "Delete",
+    "M" : "Show average price",
+    "S" : "Save the data to file",
+    "R" : "Restore data from file",
+    "Q" : "Quit this program"
+}
+
+def print_main_menu(the_menu):
+    for key, value in the_menu.items():
+        print(f"{key}: {value}")
+
+print("Welcome to Alinjos Restaurant!")
+print_main_menu(the_menu)
